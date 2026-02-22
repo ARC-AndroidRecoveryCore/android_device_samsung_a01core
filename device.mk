544d0f64184feb0764f41caa1c4c44c47b0898c0
fernandathays95-blip <fernandathays95@gmail.com>
2026-02-22 16:03:10 -0300

    Add device configuration for Galaxy A01 Core

---
+++ device.mk
@@ -0,0 +1,8 @@
+PRODUCT_DEVICE := a01core
+PRODUCT_NAME := omni_a01core
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := Galaxy A01 Core
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab
